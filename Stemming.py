6973bd634dd3f03fbf036056ac6428bbfa9a202e
Shraddha Naik <53944286+shraddhanaik15@users.noreply.github.com>
2020-10-07 22:20:34 +0530

    Stemming

---
+++ Stemming.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct  3 18:06:57 2020
+
+@author: Shraddha
+"""
+
+import nltk
+nltk.download()
+from nltk.stem import PorterStemmer
+from nltk.corpus import stopwords
+
+
+paragraph = """ In brief, the mummification process may be summarized as follows: 
+    extract, sterilize, dehydrate, perfume, seal, tag, and stock. 
+    All were done ceremoniously and with due respect to the dead body. 
+    The viscera were extracted through an incision about 10 inches long, 
+    usually made in the left side of the abdomen. 
+    Through this incision, all the “floating” contents of the abdominal cavity, 
+    namely, the stomach, the liver, the spleen, and the intestines, 
+    were removed but the kidneys were left in place. 
+    The diaphragm was then cut and the thoracic contents removed through 
+    the abdominal incision. The heart, which was considered the center of
+    emotions and the seat of conscience, was left in place. 
+    The ancient Egyptians seem to have attached no importance to the brain, 
+    which was removed through the ethmoid bone. Following these extractions began 
+    the slow process of sterilization and dehydration of the body, accomplished by 
+    osmosis with dry natron. Resterilization of the cavities, perfuming, closing the 
+    incision, and wrapping the body with linen and with beeswax completed the process.
+    Molten resin was used to seal the body and its wrappings, providing a barrier against 
+    insects and anaerobes. """
+    
+sentences = nltk.sent_tokenize(paragraph)
+stemmer = PorterStemmer()
+
+
+# Stemming
+for i in range(len(sentences)):
+    words = nltk.word_tokenize(sentences[i])
+    words = [stemmer.stem(word) for word in words if word not in set(stopwords.words('english'))]
+    sentences[i] = ' '.join(words)  
